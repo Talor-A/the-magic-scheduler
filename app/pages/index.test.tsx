f184b9e5efd550ff0408a8219cf6ea0b56a63483
Talor Anderson <ta.anders17@gmail.com>
2021-08-01 13:28:29 -0700

    fix ts error

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -16,7 +16,7 @@ test.skip("renders blitz documentation link", () => {
     id: 1,
     name: "User",
     email: "user@email.com",
-    role: "user",
+    role: "CUSTOMER",
   })
 
   const { getByText } = render(<Home />)
