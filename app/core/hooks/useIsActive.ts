240fcc279d3a374b27b7dd80569ceb4c04cf2d6e
Talor Anderson <ta.anders17@gmail.com>
2021-08-13 09:59:03 -0700

    properly highlight sidebar

--- app/core/hooks/useIsActive.ts
+++ app/core/hooks/useIsActive.ts
@@ -1,12 +1,16 @@
 import { RouteUrlObject, useRouter } from "blitz"
 
-export const useIsActive = (route?: string | RouteUrlObject) => {
+export const useIsActive = (route?: string | RouteUrlObject, exact = false) => {
   const router = useRouter()
 
   if (!route) {
     return false
   }
   if (typeof route === "string") {
+    if (!exact) return router.pathname.startsWith(route)
     return router.pathname === route
-  } else return router.pathname === route.pathname
+  } else {
+    if (!exact) return router.pathname.startsWith(route.pathname)
+    return router.pathname === route.pathname
+  }
 }
